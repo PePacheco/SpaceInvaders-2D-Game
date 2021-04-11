b86cad1bcb1b56323a8f4de1ad3fe63cfdb06dec
PePacheco <pedro_pacheco98@outlook.com>
2021-04-10 23:09:27 -0300

    Fixing conflicts

--- Edge.py
+++ Edge.py
@@ -1,45 +1,13 @@
-<<<<<<< HEAD
 class Edge:   
     def __init__(self, ini,fim):
         self.ini = ini
         self.fim = fim
         self.processed = False
-=======
-# ************************************************
-#   Edge.py
-#   Define a classe Ponto
-#   Autor: Márcio Sarroglia Pinho
-#       pinho@pucrs.br
-# ************************************************
-
-
-class Edge:   
-    def __init__(self, ini,fim,out):
-        self.ini = ini
-        self.fim = fim
-        self.out = out
->>>>>>> b9d086dcf13bab14be6122fbb6252a550b010552
         #print ("Objeto criado")
     
     def imprime(self):
         print("-------")
         self.ini.imprime()
         self.fim.imprime()
-<<<<<<< HEAD
         print("--------")
     
-=======
-        print ("Fora" ,self.out)
-        print("--------")
-    
-    def set(self, x, y, z):
-        self.ini = x
-        self.fim = y
-        self.out = z
-
-#P = Point()
-#P.set(1,2)
-#P.imprime()
-#P.set(23,34,56)
-#P.imprime()
->>>>>>> b9d086dcf13bab14be6122fbb6252a550b010552
